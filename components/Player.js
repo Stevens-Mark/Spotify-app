26fe01a52f9c3dc530608a445bdc54f6249d17da
Mark Stevens <markspstevens@yahoo.co.uk>
2023-04-29 07:52:23 +0200

    shuffle & repeat funct & graphics

--- components/Player.js
+++ components/Player.js
@@ -16,7 +16,8 @@ import {
   ArrowsRightLeftIcon,
   BackwardIcon,
   ForwardIcon,
-  ArrowUturnLeftIcon,
+  ArrowPathRoundedSquareIcon,
+  ArrowPathIcon,
   PauseCircleIcon,
   PlayCircleIcon,
 } from '@heroicons/react/24/solid';
@@ -30,13 +31,21 @@ function Player() {
   const [currentrackId, setCurrentTrackId] =
     useRecoilState(currentTrackIdState);
 
-  const skipToPrevious = async () => {
-    spotifyApi.skipToPrevious();
+  const [shuffleState, setShuffletState] = useState(false);
+  const setShuffle = () => {
+    setShuffletState((prevState) => !prevState);
+    spotifyApi
+      .setShuffle(!shuffleState)
+      .catch((err) => console.error('Shuffle failed:'));
+  };
+
+  const skipToPrevious = () => {
+    spotifyApi.skipToPrevious().catch((err) => console.error('Rewind failed:'));
     setTimeout(() => {
       spotifyApi.getMyCurrentPlayingTrack().then((data) => {
         setCurrentTrackId(data.body.item.id);
       });
-    }, '1000');
+    }, '500');
   };
 
   const handlePlayPause = () => {
@@ -54,19 +63,22 @@ function Player() {
   };
 
   const skipToNext = async () => {
-    spotifyApi.skipToNext();
+    spotifyApi
+      .skipToNext()
+      .catch((err) => console.error('Forward failed: ', err));
     setTimeout(() => {
       spotifyApi.getMyCurrentPlayingTrack().then((data) => {
         setCurrentTrackId(data.body.item.id);
       });
-    }, '1000');
+    }, '500');
   };
 
   // handles 3 way toggle, off, repeat, repeat  once
   const [repeatState, setRepeatState] = useState(0);
   const handleRepeatToggle = () => {
-    let value =
-      repeatState === 0 ? 'off' : repeatState === 1 ? 'context' : 'track';
+    let adjusted = repeatState == 0 ? 1 : repeatState == 2 ? 0 : 2;
+
+    let value = adjusted === 0 ? 'off' : adjusted === 1 ? 'context' : 'track';
     setRepeatState((prevState) => (prevState + 1) % 3);
     spotifyApi.setRepeat(`${value}`, {});
   };
@@ -97,15 +109,23 @@ function Player() {
 
   return (
     <div className="h-24 bg-gradient-to-b from-black to-gray-900 text-white text-sm md:text-base px-2 md:px-8 grid grid-cols-3">
-      {/* left hand side */}
-      <PlayingInfo />
+      <PlayingInfo /> {/* left hand side - albump hoto/info */}
       {/* center */}
       <div className="flex items-center justify-evenly">
-        <ArrowsRightLeftIcon className="button" />
-        <BackwardIcon
-          onClick={() => skipToPrevious()} // The API is not working
-          className="button"
-        />
+        <span className="relative">
+          <ArrowsRightLeftIcon
+            onClick={() => setShuffle()}
+            className={`button ${
+              shuffleState === false ? 'text-white' : 'text-green-500'
+            }`}
+          />
+          {shuffleState && (
+            <span className="absolute top-6 left-1/2 transform -translate-x-1/2 w-1 h-1 bg-green-500 rounded-full"></span>
+          )}
+        </span>
+
+        <BackwardIcon onClick={() => skipToPrevious()} className="button" />
+
         {isPlaying ? (
           <PauseCircleIcon
             className="button w-10 h-10"
@@ -117,14 +137,30 @@ function Player() {
             onClick={handlePlayPause}
           />
         )}
-        <ForwardIcon
-          onClick={() => skipToNext()} // The API is not working
-          className="button"
-        />
-        <ArrowUturnLeftIcon
-          className="button"
-          onClick={() => handleRepeatToggle()}
-        />
+
+        <ForwardIcon onClick={() => skipToNext()} className="button" />
+
+        <span className="relative">
+          <ArrowPathRoundedSquareIcon
+            onClick={() => handleRepeatToggle()}
+            className={`button w-6 h-6 ${
+              repeatState === 0 ? 'text-white' : 'text-green-500'
+            }`}
+          />
+          {repeatState === 1 || repeatState === 2 ? (
+            <span className="absolute top-6 left-1/2 transform -translate-x-1/2 w-1 h-1 bg-green-500 rounded-full"></span>
+          ) : (
+            ''
+          )}
+
+          {repeatState === 2 ? (
+            <span className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-xs text-green-500 pointer-events-none pr-[1.1px]">
+              1
+            </span>
+          ) : (
+            ''
+          )}
+        </span>
       </div>
       {/* right hand side */}
       <div className="flex items-center space-x-3 md:space-x-4 justify-end pr-5">
