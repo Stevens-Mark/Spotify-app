82553bbdd3202f00a33ade6b6264bc32c506425e
Mark Stevens <markspstevens@yahoo.co.uk>
2023-05-19 13:02:07 +0200

    sidebar pic

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import Image from 'next/image';
 import { signOut, useSession } from 'next-auth/react';
 import useSpotify from '@/hooks/useSpotify';
 // import state management recoil
@@ -24,7 +25,8 @@ function Sidebar() {
   const [playlistId, setPlaylistId] = useRecoilState(playlistIdState);
   const [currentrackId, setCurrentTrackId] =
     useRecoilState(currentTrackIdState);
-  const [activePlaylist, setActivePlaylist] = useRecoilState(activePlaylistState);
+  const [activePlaylist, setActivePlaylist] =
+    useRecoilState(activePlaylistState);
 
   useEffect(() => {
     if (spotifyApi.getAccessToken()) {
@@ -37,7 +39,11 @@ function Sidebar() {
         .then(() => {
           spotifyApi.getMyCurrentPlayingTrack().then((data) => {
             // check if the user is currently playing a track from their playlist & set page to this playlist
-            if (data.body && data.body.is_playing && data.body.context !== null) {
+            if (
+              data.body &&
+              data.body.is_playing &&
+              data.body.context !== null
+            ) {
               setCurrentTrackId(data.body.item.id);
               const currentplaylistId = data.body.context.uri.split(':');
               const playingId = currentplaylistId[currentplaylistId.length - 1];
@@ -53,56 +59,75 @@ function Sidebar() {
           );
         });
     }
-  }, [setPlaylistId, session, spotifyApi, setCurrentTrackId, setActivePlaylist]);
+  }, [
+    setPlaylistId,
+    session,
+    spotifyApi,
+    setCurrentTrackId,
+    setActivePlaylist,
+  ]);
+
+  console.log(playlists);
 
   return (
-    <div className="text-gray-500 p-5 text-xs lg:text-sm border-r border-gray-900 overflow-y-scroll h-screen scrollbar-hide  sm:w-[12rem] lg:w-[15rem] hidden md:inline-flex pb-36">
-      <div className="space-y-4">
+    <div className="text-gray-500 p-5 text-xs lg:text-sm border-r border-gray-900 overflow-y-scroll h-screen scrollbar-hide  sm:w-[15rem] lg:w-[17rem] hidden md:inline-flex pb-36">
+      <div className="space-y-4 w-full">
         <button
           className="flex items-center space-x-2 hover:text-white"
           onClick={() => signOut()}
         >
-          <ArrowLeftOnRectangleIcon className="h-5 w-5" />
+          <ArrowLeftOnRectangleIcon className="h-5 w-5 ml-3" />
           <p>Logout</p>
         </button>
         <button className="flex items-center space-x-2 hover:text-white">
-          <HomeIcon className="h-5 w-5" />
+          <HomeIcon className="h-5 w-5 ml-3" />
           <p>Home</p>
         </button>
         <button className="flex items-center space-x-2 hover:text-white">
-          <MagnifyingGlassIcon className="h-5 w-5" />
+          <MagnifyingGlassIcon className="h-5 w-5 ml-3" />
           <p>Search</p>
         </button>
         <button className="flex items-center space-x-2 hover:text-white">
-          <BuildingLibraryIcon className="h-5 w-5" />
+          <BuildingLibraryIcon className="h-5 w-5 ml-3" />
           <p>Your Library</p>
         </button>
         <hr className="border-t-[0.1px] border-gray-900" />
 
         <button className="flex items-center space-x-2 hover:text-white">
-          <PlusCircleIcon className="h-5 w-5" />
+          <PlusCircleIcon className="h-5 w-5 ml-3" />
           <p>Create Playlist</p>
         </button>
-        <button
-          className="flex items-center space-x-2 hover:text-white"
-        >
-          <HeartIcon className="h-5 w-5" />
+        <button className="flex items-center space-x-2 hover:text-white">
+          <HeartIcon className="h-5 w-5 ml-3" />
           <p>Liked Songs</p>
         </button>
         <button className="flex items-center space-x-2 hover:text-white">
-          <RssIcon className="h-5 w-5" />
+          <RssIcon className="h-5 w-5 ml-3" />
           <p>Your Episodes</p>
         </button>
         <hr className="border-t-[0.1px] border-gray-900" />
 
         {/* Playlists.. */}
         {playlists.map((playlist) => (
-          <p
+          <span
+            className={`flex items-center p-3 rounded-lg cursor-pointer ${
+              activePlaylist == playlist.id ? 'text-green-500' : 'text-white'
+            } 
+            ${
+              playlistId == playlist.id
+                ? 'bg-gray-900 hover:bg-gray-800'
+                : 'hover:bg-gray-900'
+            }`}
             key={playlist.id}
-            onClick={() => setPlaylistId(playlist.id)}
-            className="cursor-pointer hover:text-white flex items-center"
           >
-            {playlist.name}
+            <Image
+              className="h-8 w-8 mr-1 rounded-sm"
+              src={playlist.images[0].url}
+              alt="track"
+              width={100}
+              height={100}
+            />
+            <p onClick={() => setPlaylistId(playlist.id)}>{playlist.name}</p>
             <span className="pl-2">
               {activePlaylist == playlist.id ? (
                 <SpeakerWaveIcon className="w-4 h-4 text-green-500" />
@@ -110,7 +135,7 @@ function Sidebar() {
                 ' '
               )}
             </span>
-          </p>
+          </span>
         ))}
         <hr className="border-t-[0.1px] border-gray-900 pb-36" />
       </div>
