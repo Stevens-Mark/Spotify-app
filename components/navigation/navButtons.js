41317dfd1bd56641c59c68abb22edd03dcbd0f44
Mark Stevens <markspstevens@yahoo.co.uk>
2023-07-13 05:52:35 +0200

    nav button improvements

--- components/navigation/navButtons.js
+++ components/navigation/navButtons.js
@@ -3,15 +3,26 @@ import { useRouter } from 'next/router';
 // import icon/images
 import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/24/outline';
 
+/**
+ * Renders navigation forward/backward buttons
+ * @function NavigationButtons
+ * @returns {JSX}
+ */
 function NavigationButtons() {
   const router = useRouter();
-  const [canGoBack, setCanGoBack] = useState(true);
+  const [canGoBack, setCanGoBack] = useState(false);
   const [canGoForward, setCanGoForward] = useState(true);
 
-  // needs improving to mimic brower navigatyion controls
+  // still have not acheved full functionality (like on spotify)
+
+  useEffect(() => {
+    setCanGoBack(router.asPath !== '/');
+  }, [router.asPath]);
 
   const handleGoBack = () => {
-    router.back();
+    if (canGoBack) {
+      router.back();
+    }
   };
 
   const handleGoForward = () => {
@@ -20,14 +31,14 @@ function NavigationButtons() {
 
   return (
     <div className="absolute top-6 left-8 z-30 hidden xs:inline">
-      {canGoBack && (
-        <button
-          className="rounded-full mr-1 bg-gray-900 hover:bg-gray-800 text-pink-swan hover:text-white"
-          onClick={handleGoBack}
-        >
-          <ChevronLeftIcon className="p-1 w-8 h-8 pl-[1px]" />
-        </button>
-      )}
+      <button
+        className={`rounded-full mr-1 bg-gray-900 hover:bg-gray-800 text-pink-swan hover:text-white ${
+          !canGoBack && 'cursor-not-allowed'
+        }`}
+        onClick={handleGoBack}
+      >
+        <ChevronLeftIcon className="p-1 w-8 h-8 pl-[1px]" />
+      </button>
 
       {canGoForward && (
         <button
