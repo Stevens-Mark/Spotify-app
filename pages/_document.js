524d484bcf02d1b7b3bb2a89ce859493a6ead248
Mark Stevens <markspstevens@yahoo.co.uk>
2023-07-18 20:08:34 +0200

    google verify meta

--- pages/_document.js
+++ pages/_document.js
@@ -24,6 +24,7 @@ export default class MyDocument extends Document {
             name="description"
             content="Spotify Clone - A Spotify clone by Mark Stevens. A project to make a simple Spotify clone using the Spotify web api which enables the creation of applications that can interact with Spotify's streaming service, such as retrieving content metadata, getting recommendations, creating and managing playlists, or controlling playback."
           />
+          <meta name="google-site-verification" content="0jMzjXem8gLJYuM13PgdI2qiZtQljTKPzBpnDCC0KLg" />
           <meta name="author" content="Mark Stevens - Frontend Developer" />
 
           <meta property="og:title" content="Spotify clone - A simple Spotify app by Mark Stevens" />
