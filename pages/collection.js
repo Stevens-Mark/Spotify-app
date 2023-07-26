49c37b39451ffac72befcd3fcaf939804b24f551
Mark Stevens <markspstevens@yahoo.co.uk>
2023-07-26 07:59:58 +0200

    collection rewrite in progress

--- pages/collection.js
+++ pages/collection.js
@@ -4,6 +4,7 @@ import { toast } from 'react-toastify';
 import { getSession } from 'next-auth/react';
 import { useSession } from 'next-auth/react';
 // custom hooks
+import useSpotify from '@/hooks/useSpotify';
 import useScrollToTop from '@/hooks/useScrollToTop';
 import useInfiniteScroll from '@/hooks/useInfiniteScroll';
 // import state management recoil
@@ -17,51 +18,51 @@ import Footer from '@/components/navigation/Footer';
 import LikedTracks from '@/components/trackListLiked/likedTracks';
 import BackToTopButton from '@/components/backToTopButton';
 
-export async function getServerSideProps(context) {
-  const session = await getSession(context);
+// export async function getServerSideProps(context) {
+//   const session = await getSession(context);
 
-  const fetchLiked = async () => {
-    try {
-      const res = await fetch(
-        `https://api.spotify.com/v1/me/tracks?limit=${25}`,
-        {
-          headers: {
-            Authorization: `Bearer ${session.user.accessToken}`,
-          },
-        }
-      );
-      const data = await res.json();
-      // add this information to allow us to create the media banner for liked songs
-      data.id = 'collection';
-      data.type = 'collection';
-      data.name = 'Liked Songs';
-      data.owner = {
-        display_name: session?.user.name,
-        id: session?.user?.username,
-      };
-      data.images = [
-        {
-          height: 640,
-          url: '/images/LikedSongs.png',
-          width: 640,
-        },
-      ];
+//   const fetchLiked = async () => {
+//     try {
+//       const res = await fetch(
+//         `https://api.spotify.com/v1/me/tracks?limit=${25}`,
+//         {
+//           headers: {
+//             Authorization: `Bearer ${session.user.accessToken}`,
+//           },
+//         }
+//       );
+//       const data = await res.json();
+//       // add this information to allow us to create the media banner for liked songs
+//       data.id = 'collection';
+//       data.type = 'collection';
+//       data.name = 'Liked Songs';
+//       data.owner = {
+//         display_name: session?.user.name,
+//         id: session?.user?.username,
+//       };
+//       data.images = [
+//         {
+//           height: 640,
+//           url: '/images/LikedSongs.png',
+//           width: 640,
+//         },
+//       ];
+// console.log("feteched ", data)
+//       return data;
+//     } catch (err) {
+//       console.error('Error retrieving liked tracks:', err);
+//       return null;
+//     }
+//   };
 
-      return data;
-    } catch (err) {
-      console.error('Error retrieving liked tracks:', err);
-      return null;
-    }
-  };
+//   const likedTracks = await fetchLiked();
 
-  const likedTracks = await fetchLiked();
-
-  return {
-    props: {
-      likedTracks,
-    },
-  };
-}
+//   return {
+//     props: {
+//       likedTracks,
+//     },
+//   };
+// }
 
 // Helper function to merge and remove duplicates from the new data
 const mergeAndRemoveDuplicates = (existingData, newData) => {
@@ -84,19 +85,20 @@ const mergeAndRemoveDuplicates = (existingData, newData) => {
  * @param {object} likedTracks information about user's liked tracks
  * @returns {JSX}
  */
-const LikedPage = ({ likedTracks }) => {
+const LikedPage = () => {
   const { data: session } = useSession();
+  const spotifyApi = useSpotify();
   const scrollRef = useRef(null);
   const { scrollableSectionRef, showButton, scrollToTop } = useScrollToTop(); // scroll button
-  const setLikedTracklist = useSetRecoilState(likedListState);
+  const [likedTracks, setLikedTracklist] = useRecoilState(likedListState);
   const setLikedTrackUris = useSetRecoilState(likedUrisState);
   const [currentOffset, setCurrentOffset] = useState(0);
   const [stopFetch, setStopFetch] = useState(false);
 
-  useEffect(() => {
-    setLikedTracklist(likedTracks);
-    setLikedTrackUris(likedTracks?.items?.map((item) => item.track.uri)); // set uris to be used in player
-  }, [likedTracks, setLikedTrackUris, setLikedTracklist]);
+  // useEffect(() => {
+  //   setLikedTracklist(likedTracks);
+  //   setLikedTrackUris(likedTracks?.items?.map((item) => item.track.uri)); // set uris to be used in player
+  // }, [likedTracks, setLikedTrackUris, setLikedTracklist]);
 
   // show message when all data loaded/end of infinite scrolling
   useEffect(() => {
@@ -106,6 +108,42 @@ const LikedPage = ({ likedTracks }) => {
       });
     }
   }, [stopFetch]);
+
+  useEffect(() => {
+    // Get Current User's Liked Song Tracks
+    if (spotifyApi.getAccessToken()) {
+      spotifyApi
+        .getMySavedTracks({
+          limit: 25,
+          offset: 0,
+        })
+        .then(
+          function (data) {
+            // add this information to allow us to create the media banner for liked songs
+            data.body.id = 'collection';
+            data.body.type = 'collection';
+            data.body.name = 'Liked Songs';
+            data.body.owner = {
+              display_name: session?.user.name,
+              id: session?.user?.username,
+            };
+            data.body.images = [
+              {
+                height: 640,
+                url: '/images/LikedSongs.png',
+                width: 640,
+              },
+            ];
+            console.log("data ",data.body)
+            setLikedTracklist(data.body);
+            setLikedTrackUris(data.body?.items?.map((item) => item.track.uri)); // set uris to be used in player
+          },
+          function (err) {
+            console.log('Something went wrong!', err);
+          }
+        );
+    }
+  }, [session, setLikedTrackUris, setLikedTracklist, spotifyApi]);
 
   // Function to fetch more data when the user scrolls down
   const fetchMoreData = async () => {
