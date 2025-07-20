da89baeef51a1f408d722a0156812bf0259e73fb
Mark Stevens <markspstevens@yahoo.co.uk>
2025-07-20 06:40:37 +0200

    nes

--- pages/search/all.js
+++ pages/search/all.js
@@ -209,7 +209,7 @@ function All() {
             </div>
             <div className="grid grid-cols-1 xxs:grid-cols-2 xs:grid-cols-3 sm:grid-cols-4 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-7 gap-6">
               {playlists?.slice(0, numOfItems).map((item, i) => (
-                <Card key={`${item.id}-${i}`} item={item} />
+                <Card key={`${item?.id}-${i}`} item={item} />
               ))}
             </div>
           </section>
